0c11e03f3838e0854ff6aa5b50849c483102b571
Felipe Silveira <felipelarson@msn.com>
2022-05-31 20:39:20 +0100

    update: App added footer

--- linx/src/App.js
+++ linx/src/App.js
@@ -1,9 +1,11 @@
+import { FooterComponent } from './components/FooterComponent';
 import { Header } from './components/Header';
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <FooterComponent />
     </div>
   );
 }
