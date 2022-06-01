a00f6af43959be15c609bf333dd74abcbd9b4842
Felipe Silveira <felipelarson@msn.com>
2022-06-01 15:24:58 +0100

    update: Header

--- linx/src/components/Header/index.js
+++ linx/src/components/Header/index.js
@@ -1,14 +1,10 @@
-import { Container, Heading, Hero } from "react-bulma-components"
+import { Container, Heading } from "react-bulma-components"
 
 export const Header = () => {
   return (
-    <Hero color="dark">
-      <Container>
-        <Hero.Header>
-          <Heading size="3" color="light">Breweries</Heading>
-          <Heading subtitle>A breweries list by Open Brewery DB</Heading>
-        </Hero.Header>
-      </Container>
-    </Hero>
+    <Container>
+      <Heading color="light">Breweries</Heading>
+      <Heading subtitle>A breweries list by Open Brewery DB</Heading>
+    </Container>
   );
 }
