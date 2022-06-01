ad77bc0fc2c364de19f6c94531b321dd3231e3b4
Felipe Silveira <felipelarson@msn.com>
2022-06-01 15:24:42 +0100

    update: FooterComponent

--- linx/src/components/FooterComponent/index.js
+++ linx/src/components/FooterComponent/index.js
@@ -1,23 +1,15 @@
-import { Hero, Footer, Container, Content } from "react-bulma-components"
+import { Footer, Container, Content } from "react-bulma-components"
 
 export const FooterComponent = () => {
   return (
-    <div style={{ margin: '-1rem' }}>
-      <Hero size="fullheight">
-        <Hero.Header renderAs="header" />
-        <Hero.Body />
-        <Hero.Footer>
-          <Footer>
-            <Container>
-              <Content style={{ textAlign: 'center' }}>
-                <p>
-                  Breweries List
-                </p>
-              </Content>
-            </Container>
-          </Footer>
-        </Hero.Footer>
-      </Hero>
-    </div>
+    <Footer>
+      <Container>
+        <Content style={{ textAlign: 'center' }}>
+          <p>
+            Breweries List
+          </p>
+        </Content>
+      </Container>
+    </Footer>
   )
 }
