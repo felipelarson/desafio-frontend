e33e52b06c53d96b3cda8e3410349dad041271f6
Felipe Silveira <felipelarson@msn.com>
2022-06-01 15:26:49 +0100

    feat: create pages/Home

---
+++ linx/src/pages/Home/index.js
@@ -0,0 +1,89 @@
+import { Columns, Container, Hero, Pagination } from 'react-bulma-components';
+import { CardComponent } from './components/CardComponent';
+import { Filter } from './components/Filter';
+import { FooterComponent } from './components/FooterComponent';
+import { Header } from './components/Header';
+
+export const Home = () => {
+  return (
+    <div>
+      <Hero size="fullheight">
+
+        <Hero.Header>
+          <Header />
+        </Hero.Header>
+        <Container>
+
+          <Filter />
+
+          <Hero.Body>
+            <Columns>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+              <Columns.Column size={'one-quarter'}>
+                <CardComponent />
+              </Columns.Column>
+
+            </Columns>
+          </Hero.Body>
+
+          <Pagination
+            py={2}
+            current={1}
+            total={5}
+            showPrevNext={false}
+            align="center"
+          />
+
+        </Container>
+        <Hero.Footer>
+          <FooterComponent />
+        </Hero.Footer>
+
+      </Hero>
+    </div>
+  )
+}
