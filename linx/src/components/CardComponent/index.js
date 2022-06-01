d56bf95739cbb8eec1c3146c965e81a278ad0c78
Felipe Silveira <felipelarson@msn.com>
2022-06-01 15:24:11 +0100

    update: CardComponent

--- linx/src/components/CardComponent/index.js
+++ linx/src/components/CardComponent/index.js
@@ -1,22 +1,20 @@
-import { Button, Card, Content, Heading, Media } from 'react-bulma-components'
+import { Button, Card, Heading } from 'react-bulma-components'
 
 export const CardComponent = () => {
   return (
-    <Card style={{ width: 300, margin: 'auto' }}>
-      <Card.Content>
-        <Media>
-          <Media.Item>
-            <Heading size={4}>John Smith</Heading>
-          </Media.Item>
-        </Media>
-        <Content>
+    <>
+      <Card style={{ width: 300, height: 300 }}>
+        <Card.Header.Title>
+          <Heading size={4}>John Smith</Heading>
+        </Card.Header.Title>
+        <Card.Content>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
           iaculis mauris.
-        </Content>
-      </Card.Content>
-      <Card.Footer>
-        <Button size="small" color="primary">teste</Button>
-      </Card.Footer>
-    </Card>
+        </Card.Content>
+        <Card.Footer>
+          <Button size="small" color="primary">teste</Button>
+        </Card.Footer>
+      </Card>
+    </>
   )
 }
